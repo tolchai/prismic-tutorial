4965d9266967c922be2dd452f24ef95f40fb4beb
Matouš Barnat <matous.barnat@gmail.com>
2019-02-17 21:39:28 +0100

    new homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,29 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+const Index = ({ data: { prismicHomepage } }) => (
+  <React.Fragment>
+    <h1>{prismicHomepage.data.title.text}</h1>
+    <div
+      dangerouslySetInnerHTML={{ __html: prismicHomepage.data.content.html }}
+    />
+		<Link to="/second-post">Link to second post</Link>
+  </React.Fragment>
+)
+
+export default Index
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    prismicHomepage {
+      data {
+        title {
+          text
+        }
+        content {
+          html
+        }
+      }
+    }
+  }
+`
